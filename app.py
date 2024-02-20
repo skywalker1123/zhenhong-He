5bc4890bb74af79c24abafb4739036bce98c1450
skywalker1123 <137070540+skywalker1123@users.noreply.github.com>
2024-02-20 15:48:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,18 @@ from flask import Flask,request,render_template
 
 app = Flask(__name__)
 
+r = ""
+first_time =1
 @app.route("/",methods=["GET","POST"])
 def index():
   return(render_template("index.html"))
 
 @app.route("/main",methods=["GET","POST"])
 def main():
-  r = request.form.get("r")
+  global r,first_time
+  if first_time =1:
+    r = request.form.get("r")
+    first_time = 0
   return(render_template("main.html",r=r))
 
 
